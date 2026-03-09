67eb72f01d033536b83c905eb46f4716978fa79d
peblo13 <pawsli2532@gmail.com>
2026-03-09 22:52:28 +0100

    Update stripe.ts

--- vatfaktura/lib/stripe.ts
+++ vatfaktura/lib/stripe.ts
@@ -1,4 +1,4 @@
-﻿import Stripe from 'stripe';
+import Stripe from 'stripe';
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
   apiVersion: '2023-10-16',
@@ -9,15 +9,18 @@ export const SUBSCRIPTION_PLANS = {
     name: 'Free',
     price: 0,
     invoicesPerMonth: 10,
+    stripePriceId: 'price_free',
   },
   pro: {
     name: 'Pro',
     price: 9.99,
     invoicesPerMonth: 100,
+    stripePriceId: 'price_pro',
   },
   enterprise: {
     name: 'Enterprise',
     price: 29.99,
     invoicesPerMonth: -1,
+    stripePriceId: 'price_enterprise',
   },
 };
